09896486c6a00e6efc55b788706b716e717931aa
Luís Fernando de Siqueira Quissak Pereira <lquissakng@gmail.com>
2025-02-04 12:02:47 -0300

    sku per USD and BRL

--- skus_downloader.py
+++ skus_downloader.py
@@ -10,28 +10,34 @@ with open(r"files/"+today+"skus.txt", 'r') as f:
     skus = [line.rstrip() for line in f]
 
 outf = open(r"files/"+today+"sku-pricing.csv", 'w')
-outf.write("sku,price,metric,region")
+outf.write("sku|currency|price|metric|region")
 outf.write("\n")
 
-for sku in skus:
-    driver = webdriver.Firefox()
-    print("sku: ", sku)
-    driver.get("https://cloud.google.com/skus/?hl=en&filter=" +
-               sku+"&currency=BRL")
-    time.sleep(3)
-    assert 'SKU' in driver.title
+for currency in ['USD', 'BRL']:
+    for sku in skus:
+        driver = webdriver.Firefox()
+        print("sku: ", sku)
+        driver.get("https://cloud.google.com/skus/?hl=en&filter=" +
+                sku+"&currency="+currency)
+        time.sleep(3)
+        assert 'SKU' in driver.title
 
-    price = ""
-    try:
-        table = driver.find_element(by=By.TAG_NAME, value=("table"))
-        tableRow = table.find_element(by=By.XPATH, value=("//tr[1]"))
-        price = tableRow.find_element(by=By.XPATH, value=("//td[2]")).text
-        region = tableRow.find_element(by=By.XPATH, value=("//td[3]")).text
-        print(sku, price, region)
-    except:
-        print(sku, "erro !")
-    outf.write(sku + "," + price.replace(" per ", ",") + "," + region)
-    outf.write("\n")
-    driver.quit()
-    time.sleep(3)
+        price = ""
+        try:
+            table = driver.find_element(by=By.TAG_NAME, value=("table"))
+            tableRow = table.find_element(by=By.XPATH, value=("//tr[1]"))
+            price = tableRow.find_element(by=By.XPATH, value=("//td[2]")).text
+            region = tableRow.find_element(by=By.XPATH, value=("//td[3]")).text
+            splited_price = price.split("per")
+            price = splited_price[0].replace(".", ",").strip()
+            metric = splited_price[1].replace(",", "").strip()
+            price = price.replace("BRL", "").replace("USD", "").strip()
+            print(sku, price, metric, region)
+        except:
+            print(sku, "erro !")
+        outf.write(sku + "|" + currency   + "|" + price + "|" + metric + "|" + region)
+        outf.write("\n")
+        driver.quit()
+        time.sleep(3)
 outf.close()
+
